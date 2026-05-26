fb53be3ec439969718c0fd6d9cf43bc3d95481b2
caua-mourawx <cauajoseferreiramoura@gmail.com>
2026-05-26 20:36:41 -0300

    Update Midia.php

---
+++ classes/Midia.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+abstract class Midia
+{
+    public function __construct(
+        private string $titulo,
+        private int $duracao
+    ) {
+    }
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function getDuracao(): int
+    {
+        return $this->duracao;
+    }
+
+    abstract public function reproduzir(): string;
+}
+
+class Musica extends Midia
+{
+    public function __construct(
+        string $titulo,
+        int $duracao,
+        private string $artista
+    ) {
+        parent::__construct($titulo, $duracao);
+    }
+
+    public function reproduzir(): string
+    {
+        return "Tocando agora: " . $this->getTitulo() .
+            " - " . $this->artista .
+            " (" . $this->getDuracao() . " segundos)";
+    }
+}
+
+$musica = new Musica(
+    "Call Out My Name",
+    228,
+    "The Weeknd"
+);
+
+echo $musica->reproduzir();
